754abf7ea83c3ed12613d4a7064d8f674367517a
Ext-E.Nenakhov <enenakhov@luxoft.com>
2020-03-21 10:39:44 +0300

    Добавьте файлы проекта.

---
+++ Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons
+{
+    class Car : IComparable
+    {
+        public Car(String id)
+        {
+            this.CarID = id;
+        }
+        public String CarID { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            Car temp = obj as Car;
+            if (temp != null)
+                return CarID.CompareTo(temp.CarID);
+            else
+                throw new ArgumentException("Comparable object is not a class Car");
+        }
+
+        public override string ToString()
+        {
+            return this.CarID;
+        }
+    }
+}
---
+++ CarComparer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons
+{
+    class CarComparer<T> : IComparer<Car>
+    {
+        public int Compare(Car x, Car y)
+        {
+            return x.CompareTo(y) * -1;
+        }
+    }
+}
